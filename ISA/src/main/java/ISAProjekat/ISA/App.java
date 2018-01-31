6cc85028c22d56948c7aeee4da19922621d513c1
StefanMilanovic <milanovicstefann@gmail.com>
2018-01-31 16:50:04 +0100

    1

---
+++ ISA/src/main/java/ISAProjekat/ISA/App.java
@@ -0,0 +1,13 @@
+package ISAProjekat.ISA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
